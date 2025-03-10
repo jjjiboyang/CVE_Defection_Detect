e02b51faf7d7c77391553f58dff600fb86b436e0
吉博洋 <jjjiboyang@gmail.com>
2025-03-10 18:52:05 +0800

    添加大区域检测；只要检测到一种缺陷就可以退出当前循环；

--- Algorithm/ProcessImage.py
+++ Algorithm/ProcessImage.py
@@ -21,9 +21,9 @@ def source_path(relative_path):
 
 
 class Detect:
-    def __init__(self,defect_types):
-        model1=str(source_path(os.path.join("res","model_water.hdl")))
-        model2=str(source_path(os.path.join("res","model_black.hdl")))
+    def __init__(self, defect_types):
+        model1 = str(source_path(os.path.join("res", "model_water.hdl")))
+        model2 = str(source_path(os.path.join("res", "model_black.hdl")))
 
         # 读取神经网络模型
         self.DLModelHandle_water = ha.read_dl_model(model1)
@@ -33,7 +33,7 @@ class Detect:
         ha.set_dl_model_param(self.DLModelHandle_black, 'runtime', 'gpu')
         # 生成一个样本字典
         self.DLSample = ha.create_dict()
-        self.DLSample_black= ha.create_dict()
+        self.DLSample_black = ha.create_dict()
         # 需要剔除的缺陷种类
         self.defect_type_1 = defect_types[0]
         self.defect_type_2 = defect_types[1]
@@ -41,10 +41,10 @@ class Detect:
         self.defect_type_4 = defect_types[3]
 
     def detect(self, Image, filename):
-        FOLDER=f"./All_Images/{(datetime.now().strftime('%Y-%m-%d'))}"
-        result=""
+        FOLDER = f"./All_Images/{(datetime.now().strftime('%Y-%m-%d'))}"
+        result = ""
         RectanglePoints = []
-        ImageScaled=ha.scale_image(Image,1.5,-50)
+        ImageScaled = ha.scale_image(Image, 1.5, -50)
         ImageMedian = ha.median_image(ImageScaled, 'circle', 3, 'mirrored')
         # 计算偏差图
         ImageDeviation = ha.deviation_image(ImageMedian, 3, 21)
@@ -52,71 +52,98 @@ class Detect:
         Region = ha.var_threshold(ImageDeviation, 30, 30, 0.2, 6, 'light')
         # 连通域分析
         ConnectedRegions = ha.connection(Region)
+        # 填充连通域
+        RegionFillUp = ha.fill_up(ConnectedRegions)
 
-        Area, Row, Column = ha.area_center(ConnectedRegions)
-        Height, Width, Ratio=ha.height_width_ratio(ConnectedRegions)
-        gray_min, gray_max, Range=ha.min_max_gray(ConnectedRegions,Image,0)
-        gray_mean, Deviation=ha.intensity(ConnectedRegions,Image)
+        Area, Row, Column = ha.area_center(RegionFillUp)
+        Height, Width, Ratio = ha.height_width_ratio(RegionFillUp)
+        gray_min, gray_max, Range = ha.min_max_gray(RegionFillUp, Image, 0)
+        gray_mean, Deviation = ha.intensity(RegionFillUp, Image)
 
         # 定义规则类别
         # 长条划痕
-        if self.defect_type_1==0:
-            class1=[0]
+        if self.defect_type_1 == 0:
+            class1 = [0]
         else:
-            aa=ha.tuple_greater_elem(Area,400)
-            bb=ha.tuple_greater_elem(Height,600)
-            cc=ha.tuple_greater_elem(2000,Height)
-            dd=ha.tuple_greater_elem(10,Deviation)
+            aa = ha.tuple_greater_elem(Area, 400)
+            bb = ha.tuple_greater_elem(Height, 600)
+            cc = ha.tuple_greater_elem(2000, Height)
+            dd = ha.tuple_greater_elem(10, Deviation)
             class1 = list(map(lambda x, y, z, w: x & y & z & w, aa, bb, cc, dd))
 
-        # 晶圆
-        if self.defect_type_2==0:
-            class2=[0]
+        # 水斑
+        if self.defect_type_2 == 0:
+            class2 = [0]
         else:
-            a=ha.tuple_greater_elem(Area,600)
-            b=ha.tuple_greater_elem(3000,Area)
-            c=ha.tuple_greater_elem(gray_max,200)
-            d=ha.tuple_greater_elem(gray_mean,150)
-            e=ha.tuple_greater_elem(100,Height)
-            class2=list(map(lambda x, y, z, w, t : x & y & z & w & t, a, b, c, d,e))
+            a = ha.tuple_greater_elem(Area, 600)
+            b = ha.tuple_greater_elem(5000, Area)
+            c = ha.tuple_greater_elem(gray_max, 200)
+            d = ha.tuple_greater_elem(gray_mean, 150)
+            e = ha.tuple_greater_elem(100, Height)
+            class2 = list(map(lambda x, y, z, w, t: x & y & z & w & t, a, b, c, d, e))
 
         # 糊料
-        if self.defect_type_3==0:
-            class3=[0]
+        if self.defect_type_3 == 0:
+            class3 = [0]
+            class5 = [0]
         else:
-            a=ha.tuple_greater_elem(Area,50)
-            b=ha.tuple_greater_elem(90,gray_min)
-            c=ha.tuple_greater_elem(150,gray_mean)
-            d=ha.tuple_greater_elem(2000, Height)
+            a = ha.tuple_greater_elem(Area, 120)
+            b = ha.tuple_greater_elem(90, gray_min)
+            c = ha.tuple_greater_elem(150, gray_mean)
+            d = ha.tuple_greater_elem(2000, Height)
             class3 = list(map(lambda x, y, z, w: x & y & z & w, a, b, c, d))
 
+            # 特大缺陷
+            a = ha.tuple_greater_elem(Area, 5000)
+            b = ha.tuple_greater_elem(Height, 2000)
+            class5 = list(map(lambda x, y: x & y, a, b))
+
         # 连续晶圆和塑化不良
-        if self.defect_type_4==0:
-            class4=[0]
+        if self.defect_type_4 == 0:
+            class4 = [0]
         else:
-            a=ha.tuple_greater_elem(Area,300)
-            b=ha.tuple_greater_elem(600,Area)
-            c=ha.tuple_greater_elem(100,Height)
+            a = ha.tuple_greater_elem(Area, 300)
+            b = ha.tuple_greater_elem(600, Area)
+            c = ha.tuple_greater_elem(100, Height)
             class4 = list(map(lambda x, y, z: x & y & z, a, b, c))
 
-        sum1=ha.tuple_sum(class1)[0]
-        sum2=ha.tuple_sum(class2)[0]
-        sum3=ha.tuple_sum(class3)[0]
-        sum4=ha.tuple_sum(class4)[0]
+        sum1 = ha.tuple_sum(class1)[0]
+        sum2 = ha.tuple_sum(class2)[0]
+        sum3 = ha.tuple_sum(class3)[0]
+        sum4 = ha.tuple_sum(class4)[0]
+        sum5 = ha.tuple_sum(class5)[0]
 
         # 提取每类索引
-        class1_index=ha.tuple_find(class1,1)
-        class1_index=[x+1 for x in class1_index]
-        class2_index=ha.tuple_find(class2,1)
-        class2_index=[x+1 for x in class2_index]
-        class3_index=ha.tuple_find(class3,1)
-        class3_index=[x+1 for x in class3_index]
-        class4_index=ha.tuple_find(class4,1)
-        class4_index=[x+1 for x in class4_index]
+        class1_index = ha.tuple_find(class1, 1)
+        class1_index = [x + 1 for x in class1_index]
+        class2_index = ha.tuple_find(class2, 1)
+        class2_index = [x + 1 for x in class2_index]
+        class3_index = ha.tuple_find(class3, 1)
+        class3_index = [x + 1 for x in class3_index]
+        class4_index = ha.tuple_find(class4, 1)
+        class4_index = [x + 1 for x in class4_index]
+        class5_index = ha.tuple_find(class5, 1)
+        class5_index = [x + 1 for x in class5_index]
+
+        # 如果有特大缺陷
+        if sum5 > 0:
+            Region5 = ha.select_obj(RegionFillUp, class5_index)
+            # 找到最小外接矩形
+            Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(Region5)
+            # 创建矩形区域
+            Rectangle = ha.gen_rectangle1(Row1, Column1, Row2, Column2)
+            # 膨胀矩形区域
+            RegionDilation = ha.dilation_rectangle1(Rectangle, 32, 32)
+            # 重新计算最小外接矩形
+            Row11, Column11, Row21, Column21 = ha.smallest_rectangle1(RegionDilation)
+            for K in range(1, sum1 + 1):
+                # 传入缺陷区域矩形参数
+                RectanglePoints.append([Row11[K - 1], Column11[K - 1], Row21[K - 1], Column21[K - 1]])
+            return "3", RectanglePoints
 
         # 提取区域
-        if sum1>0:
-            Region1=ha.select_obj(ConnectedRegions,class1_index)
+        if sum1 > 0:
+            Region1 = ha.select_obj(RegionFillUp, class1_index)
             # 找到最小外接矩形
             Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(Region1)
             # 创建矩形区域
@@ -131,7 +158,7 @@ class Detect:
             # 判断有无水珠
             ImageConverted = ha.convert_image_type(ImageCrop, 'real')
             ImageZoom = ha.zoom_image_size(ImageConverted, 40, 100, 'constant')
-            ImageScaled1 = ha.scale_image(ImageZoom,1,-127)
+            ImageScaled1 = ha.scale_image(ImageZoom, 1, -127)
 
             for K in range(1, sum1 + 1):
                 # 选择对象
@@ -143,7 +170,7 @@ class Detect:
                 ImageClass = ha.get_dict_tuple(DLResult[0], 'classification_class_ids')
                 if not ImageClass[0]:
                     # 传入缺陷区域矩形参数
-                    RectanglePoints.append([Row11[K-1], Column11[K-1], Row21[K-1], Column21[K-1]])
+                    RectanglePoints.append([Row11[K - 1], Column11[K - 1], Row21[K - 1], Column21[K - 1]])
                     # 保存图像
                     os.makedirs(f"{FOLDER}/out_long", exist_ok=True)
                     output_path = f"{FOLDER}/out_long/{filename}_{K}.png"
@@ -155,12 +182,11 @@ class Detect:
                     # ha.write_image(ImageSave, 'png', 0, output_path)
                     sum1 -= 1
 
-            if sum1>0:
-                result+="1"
+            if sum1 > 0:
+                return "1", RectanglePoints
 
-
-        if sum2>0:
-            Region2=ha.select_obj(ConnectedRegions,class2_index)
+        if sum2 > 0:
+            Region2 = ha.select_obj(RegionFillUp, class2_index)
             # 找到最小外接矩形
             Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(Region2)
             # 创建矩形区域
@@ -199,11 +225,11 @@ class Detect:
                     # ha.write_image(ImageSave, 'png', 0, output_path)
                     sum2 -= 1
 
-            if sum2>0:
-                result+="2"
+            if sum2 > 0:
+                return "2", RectanglePoints
 
         if sum3 > 0:
-            Region3 = ha.select_obj(ConnectedRegions, class3_index)
+            Region3 = ha.select_obj(RegionFillUp, class3_index)
             # 找到最小外接矩形
             Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(Region3)
             # 创建矩形区域
@@ -251,14 +277,12 @@ class Detect:
                     # ha.write_image(ImageSave, 'png', 0, output_path)
                     sum3 -= 1
 
-            if sum3>0:
-                result+="3"
-
-
+            if sum3 > 0:
+                return "3", RectanglePoints
 
         if sum4 >= 6:
-            count=0
-            Region4 = ha.select_obj(ConnectedRegions, class4_index)
+            count = 0
+            Region4 = ha.select_obj(RegionFillUp, class4_index)
             # 找到最小外接矩形
             Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(Region4)
             # 创建矩形区域
@@ -284,33 +308,29 @@ class Detect:
                 DLResult = ha.apply_dl_model(self.DLModelHandle_water, [self.DLSample], [])
                 ImageClass = ha.get_dict_tuple(DLResult[0], 'classification_class_ids')
                 if not ImageClass[0]:
-                    count+=1
-                    if count>=6:
-                        break
                     # 传入缺陷区域矩形参数
                     RectanglePoints.append([Row11[K - 1], Column11[K - 1], Row21[K - 1], Column21[K - 1]])
                     # 保存图像
                     os.makedirs(f"{FOLDER}/out_continue", exist_ok=True)
                     output_path = f'{FOLDER}/out_continue/{filename}_{K}.png'
                     ha.write_image(ImageSave, 'png', 0, output_path)
-
+                    count += 1
+                    if count >= 6:
+                        return "4", RectanglePoints
                 else:
                     # os.makedirs(f"{FOLDER}/out_water", exist_ok=True)
                     # output_path = f'{FOLDER}/out_water/{filename}_{K}.png'
                     # ha.write_image(ImageSave, 'png', 0, output_path)
                     sum4 -= 1
 
-            if sum4 >= 6:
-                result += "4"
-
         if result == "":
             return "0"
 
-        return result,RectanglePoints
+        return result, RectanglePoints
 
 
 class ProcessImage:
-    def __init__(self, save_choice, light_queue, image_encoder_queue,defect_types):
+    def __init__(self, save_choice, light_queue, image_encoder_queue, defect_types):
         self.img_queue = queue.Queue()
         self.light_queue = light_queue
         self.save_choice = save_choice
@@ -397,6 +417,6 @@ class ProcessImage:
                 # print("total:", float(end_t - start_t) * 1000.0, "ms")
 
 
-def run_ImageProcessing(save_choice, light_queue, image_encoder_queue,defect_types):
-    process = ProcessImage(save_choice, light_queue, image_encoder_queue,defect_types)
+def run_ImageProcessing(save_choice, light_queue, image_encoder_queue, defect_types):
+    process = ProcessImage(save_choice, light_queue, image_encoder_queue, defect_types)
     process.detect_defects()
