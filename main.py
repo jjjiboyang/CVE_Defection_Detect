8889a0515ace39dca057d83b380fb9ab782b47f0
吉博洋 <jjjiboyang@gmail.com>
2025-03-11 09:39:08 +0800

    不删除历史图片

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ if __name__ == "__main__":
     #     exit(0)
     if os.path.isfile("log.txt"):
         os.remove("log.txt")
-    if os.path.isdir("./All_Images"):
-        shutil.rmtree("./All_Images")
-    if os.path.isdir("./data_lmdb"):
-        shutil.rmtree("./data_lmdb")
+    # if os.path.isdir("./All_Images"):
+    #     shutil.rmtree("./All_Images")
+    # if os.path.isdir("./data_lmdb"):
+    #     shutil.rmtree("./data_lmdb")
 
     logger = logger_config()
 
